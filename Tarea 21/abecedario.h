e2813fb13b8f41b509261b0a86fa7b12dd98ad97
cfonseca2 <31553514+cfonseca2@users.noreply.github.com>
2017-11-09 17:50:46 -0600

    Tarea 21

---
+++ Tarea 21/abecedario.h
@@ -0,0 +1,41 @@
+//letras.h
+
+void letraC(){
+	printf("CCCC\n");
+	printf("C   \n");
+	printf("C   \n");
+	printf("C   \n");
+	printf("CCCC\n");
+	}
+
+void letraE(){
+	printf("EEEE\n");
+	printf("E   \n");
+	printf("EEE \n");
+	printf("E   \n");
+	printf("EEEE\n");
+	}
+	
+void letraS(){
+	printf("SSSS\n");
+	printf("S   \n");
+	printf("SSSS\n");
+	printf("   S\n");
+	printf("SSSS\n");
+	}
+
+void letraA(){
+	printf(" AA \n");
+	printf("A  A\n");
+	printf("AAAA\n");
+	printf("A  A\n");
+	printf("A  A\n");
+}
+
+void letraR(){
+	printf("RRRR\n");
+	printf("R  R\n");
+	printf("RRR \n");
+	printf("R  R\n");
+	printf("R   R\n");
+}
---
+++ Tarea 21/nombre.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "abecedario.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	letraC();
+	printf("\n");
+	letraE();
+	printf("\n");
+	letraS();
+	printf("\n");
+	letraA();
+	printf("\n");
+	letraR();
+	
+	return 0;
+}
